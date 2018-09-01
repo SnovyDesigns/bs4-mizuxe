e5e7248ec56e45812fb69bd242c6f72d3ecc5779
Krzysztof Piekarz <krzysztofpiekarz@iMac-Krzysztof.local>
2018-09-01 11:27:03 +0200

    Finished

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,24 +2,15 @@ import $ from 'jquery';
 
 $('#year').text(new Date().getFullYear());
 
-$('body').scrollspy({ target: '#main-nav' });
+$('body').scrollspy({ target: '#main-nav', offset: 110 });
 
 $('#main-nav a').on('click', function(e) {
-  // Check for a hash value
-  if (this.hash !== '') {
-    // Prevent default behaviour
-    e.preventDefault();
-  }
   const hash = this.hash;
 
   $('html, body').animate(
     {
-      scrollTop: $(hash).offset().top
+      scrollTop: $(hash).offset().top - 110
     },
-    900,
-    function() {
-      // Add hash to URL after scroll
-      window.location.hash = hash;
-    }
+    900
   );
 });
